6b9a1e46ecddd1af0377e63281758f222355a7e5
LukeDitria <luke.ditria@monash.edu>
2024-07-20 12:28:50 +1000

    fix save dir location

--- start_capture.sh
+++ start_capture.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+source /home/luke/Documents/nanobox/bin/activate
 cd /home/luke/Documents/capture_box
 python3 capture_run.py --input_dir /media/usbdrive/motion_images --output_dir /media/usbdrive/yolo_outputs --interval 300 --confidence 0.35
