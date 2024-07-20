517bb7fad8a6582d36e491143fcd8f9a19eb9e8e
LukeDitria <luke.ditria@monash.edu>
2024-07-20 12:02:40 +1000

    save jsons and images to dirs

--- capture_run.py
+++ capture_run.py
@@ -15,7 +15,7 @@ def parse_arguments():
     parser.add_argument("--input_dir", type=str, required=True, help="Directory to scan for new images")
     parser.add_argument("--output_dir", type=str, required=True, help="Directory to move processed images")
     parser.add_argument("--interval", type=int, default=300, help="Scan interval in seconds (default: 300)")
-    parser.add_argument("--confidence", type=float, default=0.5, help="Confidence threshold (default: 0.5)")
+    parser.add_argument("--confidence", type=float, default=0.35, help="Confidence threshold (default: 0.5)")
     parser.add_argument("--image_size", type=int, default=640, help="Size to resize image")
     return parser.parse_args()
 
@@ -31,17 +31,25 @@ def main():
     ort_session = onnxruntime.InferenceSession("./yolov7-tiny.onnx",
                                                providers=['CUDAExecutionProvider'])
 
+    json_detections_path = os.path.join(args.output_dir, "detections")
+    if not os.path.exists(json_detections_path):
+        os.mkdir(json_detections_path)
+
+    image_detections_path = os.path.join(args.output_dir, "images")
+    if not os.path.exists(image_detections_path):
+        os.mkdir(image_detections_path)
+
     while True:
         for filename in os.listdir(args.input_dir):
             if filename.lower().endswith('.jpg'):
                 image_path = os.path.join(args.input_dir, filename)
-                coordinates, label, confidence = utils.process_image(image_path, args.image_size, ort_session,
-                                                                     yolo_labels, args.confidence)
+                coordinates, label, confidence = utils.process_image(image_detections_path, args.image_size,
+                                                                     ort_session, yolo_labels, args.confidence)
 
                 if coordinates is not None:
                     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
                     new_filename = f"{label}-{timestamp}-1.jpg"
-                    new_path = os.path.join(args.output_dir, new_filename)
+                    new_path = os.path.join(json_detections_path, new_filename)
                     shutil.move(image_path, new_path)
                     utils.log_detection(new_path, label, confidence, coordinates, timestamp)
                     print(f"Detected {label} with confidence {confidence:.2f}")
