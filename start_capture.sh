cabdd267d549625f9d9a169742d70d524ba1c7e3
LukeDitria <luke.ditria@monash.edu>
2024-07-20 15:05:00 +1000

    update

--- start_capture.sh
+++ start_capture.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Needed to setup automatic mounting of usb in fstab
 source /home/luke/Documents/nanobox/bin/activate
 cd /home/luke/Documents/capture_box
-python3 capture_run.py --input_dir /media/usbdrive/motion_images --output_dir /media/usbdrive/yolo_outputs --interval 300 --confidence 0.5
+python3 capture_run.py --input_dir /media/usb/motion_images --output_dir /media/usb/yolo_outputs --interval 300 --confidence 0.5
