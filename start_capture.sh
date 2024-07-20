bac1081d33e980932d2ae87e9bc52485783f603c
LukeDitria <luke.ditria@monash.edu>
2024-07-20 12:03:10 +1000

    setup for service

---
+++ start_capture.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/luke/Documents/capture_box
+python3 capture_run.py --input_dir /media/usbdrive/motion_images --output_dir /media/usbdrive/yolo_outputs --interval 300 --confidence 0.35
