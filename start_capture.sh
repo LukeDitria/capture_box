be43bf75a57d36e14b76e75eab55d56090b069d4
LukeDitria <luke.ditria@monash.edu>
2024-07-20 15:33:48 +1000

    just use python

--- start_capture.sh
+++ start_capture.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+# Make this file runnable chmod +x start_capture.sh
 #Needed to setup automatic mounting of usb in fstab
 source /home/luke/Documents/nanobox/bin/activate
 cd /home/luke/Documents/capture_box
-python3 capture_run.py --input_dir /media/usb/motion_images --output_dir /media/usb/yolo_outputs --interval 300 --confidence 0.5
+python capture_run.py --input_dir /media/usb/motion_images --output_dir /media/usb/yolo_outputs --interval 300 --confidence 0.5
